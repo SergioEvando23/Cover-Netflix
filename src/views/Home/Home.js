8aa1879bae9509c38991aa2c7be82311a9deeb98
SergioCosta23 <sergioevandocosta@gmail.com>
2021-06-22 18:25:43 -0300

    home page generate

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -2,11 +2,13 @@ import React, {useEffect, useState} from 'react';
 import Tmdb from '../../Tmdb';
 
 const Home = ( ) => {
+    const [moveList, setMoveList] = useState([]);
 
     useEffect(() => {
         const loadAll = async () => {
           //pegando a lista total
           let list = await Tmdb.getHomeList();
+          console.log(list);
         }
         loadAll();
     }, [])
