34c1460501ac71925592b99e1676a39aca86ac80
SergioCosta23 <sergioevandocosta@gmail.com>
2021-06-24 14:56:48 -0300

    added description and informations for items

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -1,3 +1,5 @@
+import { KeyboardReturnSharp } from "@material-ui/icons";
+
 const API_KEY = '7d498c0116d3e86a063b6a5ebf312c61';
 const API_BASE = 'https://api.themoviedb.org/3';
 
@@ -70,6 +72,23 @@ export default {
                 items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
             },
             
-        ]
+        ];
+    },
+    getMovieInfo: async( movieId, type ) => {
+        let info = {};
+        if(movieId){
+            switch(type) {
+                case 'movie':
+                    info = await basicFetch(`/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+                break;
+                case 'tv':
+                    info = await basicFetch(`/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+                break;
+                default:
+                    info = null;
+                break;
+            }
+        }
+        return info;
     }
 };
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -2,28 +2,40 @@ import React, {useEffect, useState} from 'react';
 import Tmdb from '../../Tmdb';
 import './Home.css';
 import MovieRow from '../../components/MovieRow/MovieRow';
+import FeaturedMovie from '../../components/FeaturedMovie/FeaturedMovie';
 
 const Home = ( ) => {
     const [movieList, setMovieList] = useState([]);
+    const [featureData, setFeatureData] = useState(null);
     
     useEffect(() => {
         const loadAll = async () => {
           //pegando a lista total
           let list = await Tmdb.getHomeList();
           setMovieList(list);
+
+          //pegando o fieature
+          let originals =  list.filter(i=>i.slug === 'originals');
+          let randomChonsen = Math.floor(Math.random() * (originals[0].items.results.length -1));
+          let chosen = originals[0].items.results[randomChonsen];
+          let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+          setFeatureData(chosenInfo);
         }
         loadAll();
     }, []);
 
     return (
         <div className="home">
-          <section className="lists">
-              {movieList.map((item) =>(
-                  <div key={item.id}>
-                      <MovieRow key={item.id} title={item.title} items={item.items} />
-                  </div>
-              ))}
-          </section>
+            {featureData && 
+                <FeaturedMovie item={featureData}/>
+            }
+            <section className="lists">
+                {movieList.map((item) =>(
+                    <div key={item.id}>
+                        <MovieRow key={item.id} title={item.title} items={item.items} />
+                    </div>
+                ))}
+            </section>
         </div>
     )
 }
