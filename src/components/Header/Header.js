82bba1066c8a836d3b99f41f3d580256c4fcabc7
SergioCosta23 <sergioevandocosta@gmail.com>
2021-06-24 22:01:44 -0300

    responsiveness successfully done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,20 +4,10 @@ import './Header.css';
 const Header = ({black}) => {
     return (
         <header className={black ? "black" : ""}>
-            <div className="hearder--container--logo"> 
-                <div className="header--logo">
-                    <a href="/">
-                        <img src="https://logodownload.org/wp-content/uploads/2014/10/netflix-logo-5.png" alt="Netflix-logo" />
-                    </a>
-                </div>
-                <dl className="header--list">
-                    <dd className="header--list--item"> Início </dd>
-                    <dd className="header--list--item"> Assistir de novo </dd>
-                    <dd className="header--list--item"> Séries </dd>
-                    <dd className="header--list--item"> Filmes </dd>
-                    <dd className="header--list--item"> Bombando </dd>
-                    <dd className="header--list--item"> Minha lista </dd>
-                </dl>
+            <div className="header--logo">
+                <a href="/">
+                    <img src="https://logodownload.org/wp-content/uploads/2014/10/netflix-logo-5.png" alt="Netflix-logo" />
+                </a>
             </div>
             <div className="header--user">
                 <a href="/">
